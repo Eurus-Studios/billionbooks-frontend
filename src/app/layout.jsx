4732d4eba6086acd9f12a6f1661ee87135f12443
Suchanapal <122770752+Suchanapal@users.noreply.github.com>
2023-07-24 23:56:09 +0530

    updated layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,10 @@
-"use client"
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Navbar from '@/components/Navbar';
-import MobileNav from '@/components/MobileNav';
 import Ft from "@/components/Ft";
 
 import StickySocials from '@/components/StickySocials';
+import CombinedNav from '@/components/CombinedNav';
 
 
 
@@ -18,26 +16,11 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const [windowWidth, setWindowWidth] = useState(0);
-
-  useEffect(() => {
-    
-    setWindowWidth(window.innerWidth);
-
-    const handleResize = () => setWindowWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const isMobile = windowWidth < 950;
-
   return (
     <html lang="en">
       <body className={inter.className}>
       
-        {isMobile ? <MobileNav /> : <Navbar />}
+        <CombinedNav/>
         <StickySocials />
         {children}
         <Ft/>
